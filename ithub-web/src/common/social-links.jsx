bc7821bcec002e7594035bf51b167bb54108112a
Văn Hiệp <112191296+vhiepp@users.noreply.github.com>
2024-05-19 01:03:08 +0700

    chore: Add FooterTwo component to MainLayout and update social links

--- ithub-web/src/common/social-links.jsx
+++ ithub-web/src/common/social-links.jsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
 
-
 const social_links = [
   {
     link: "http://facebook.com",
     target: "_blank",
-    icon: "fab fa-facebook-f", 
+    icon: "fab fa-facebook-f",
   },
   {
     link: "http://twitter.com",
     target: "_blank",
-    icon: "fab fa-twitter", 
+    icon: "fab fa-twitter",
   },
   {
     link: "https://www.linkedin.com",
     target: "_blank",
-    icon: "fab fa-linkedin-in", 
-  }, 
+    icon: "fab fa-linkedin-in",
+  },
   {
     link: "https://www.vimeo.com",
     target: "_blank",
-    icon: "fab fa-vimeo-v", 
+    icon: "fab fa-vimeo-v",
   },
 ];
 
@@ -28,12 +27,7 @@ const SocialLinks = () => {
   return (
     <>
       {social_links.map((l, i) => (
-        <Link
-          key={i}
-          href={l.link}
-          className={l.color}
-          target={l.target ? l.target : ""}
-        >
+        <Link key={i} href={l.link} className={l.color} target={l.target ? l.target : ""}>
           <i className={l.icon}></i>
         </Link>
       ))}
@@ -43,26 +37,22 @@ const SocialLinks = () => {
 
 export default SocialLinks;
 
-
-
-
-
-
 const copy_right_text = {
-  copy_right: <>Full Copyright & Design By <Link href="#">@Theme pure</Link> – {new Date().getFullYear()}</>,
-}
-const {copy_right}  = copy_right_text
-
-export const CopyRight = ()  => {
-  return (
+  copy_right: (
     <>
-    {copy_right}
+      Copyright By{" "}
+      <Link href="https://github.com/TVU-ITHub" target="_blank">
+        @ITHub Club
+      </Link>{" "}
+      – {new Date().getFullYear()}
     </>
-  )
-}
+  ),
+};
+const { copy_right } = copy_right_text;
 
-
-
+export const CopyRight = () => {
+  return <>{copy_right}</>;
+};
 
 // home 04 social link
 const social_links_two = [
@@ -70,43 +60,38 @@ const social_links_two = [
     link: "http://facebook.com",
     target: "_blank",
     icon: "fab fa-facebook-f",
-    color: "1", 
+    color: "1",
   },
   {
-      link: "https://www.instagram.com",
-      target: "_blank",
-      icon: "fab fa-instagram", 
-      color: "2",
+    link: "https://www.instagram.com",
+    target: "_blank",
+    icon: "fab fa-instagram",
+    color: "2",
   },
 
   {
     link: "http://twitter.com",
     target: "_blank",
-    icon: "fab fa-twitter", 
+    icon: "fab fa-twitter",
     color: "3",
   },
   {
     link: "https://www.linkedin.com",
     target: "_blank",
     icon: "fab fa-linkedin-in",
-    color: "4", 
-  }, 
+    color: "4",
+  },
 ];
 
-export const  SocialLinksTwo = ()  => {
-   return (
-     <>
-        {social_links_two.map((link, i) => (
-          <Link
-            key={i}
-            target={link.target}
-            className={`icon-color-${link.color}`}
-            href={link.link}
-          >
-            <i className={link.icon}></i>
-            <span></span>
-          </Link>
-        ))}
-     </>
-   );
-}
+export const SocialLinksTwo = () => {
+  return (
+    <>
+      {social_links_two.map((link, i) => (
+        <Link key={i} target={link.target} className={`icon-color-${link.color}`} href={link.link}>
+          <i className={link.icon}></i>
+          <span></span>
+        </Link>
+      ))}
+    </>
+  );
+};
--- ithub-web/src/layout/main-layout.jsx
+++ ithub-web/src/layout/main-layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeaderFour from "./headers/header-4";
+import FooterTwo from "./footers/footer-2";
 
 function MainLayout({ children, white_header = false, style_error = true }) {
   return (
     <>
       <HeaderFour white_header={white_header} style_error={style_error} />
       {children}
+      <FooterTwo />
     </>
   );
 }
