ff975074f87e8884fa84d9fbfbe50fefcc54f4fe
Văn Hiệp <112191296+vhiepp@users.noreply.github.com>
2024-05-22 01:28:41 +0700

    chore: Reorder about_list items in about-area component

--- ithub-web/src/components/homes/home/feature-area.jsx
+++ ithub-web/src/components/homes/home/feature-area.jsx
@@ -87,7 +87,7 @@ const FeatureArea = () => {
                   </h3>
                   <p>{item.sub_title}</p>
                   <div className="tp-feature__link tp-common-btn">
-                    <Link href="/service-details">
+                    <Link href="#">
                       <RightArrow />
                     </Link>
                   </div>
--- ithub-web/src/data/portfolio-blog.js
+++ ithub-web/src/data/portfolio-blog.js
@@ -1,4 +1,4 @@
-// thumb img import here 
+// thumb img import here
 import thumb_img_1 from "../../public/assets/img/blog/blog-grid-1.jpg";
 import thumb_img_2 from "../../public/assets/img/blog/blog-grid-2.jpg";
 import thumb_img_3 from "../../public/assets/img/blog/blog-grid-3.jpg";
@@ -13,7 +13,6 @@ import thumb_img_11 from "../../public/assets/img/blog/blog-grid-5.jpg";
 import thumb_img_12 from "../../public/assets/img/blog/blog-grid-6.jpg";
 
 // avata img import here
-import avata_img_1 from "../../public/assets/img/avata/avata-1.png";
 import avata_img_2 from "../../public/assets/img/avata/avata-2.png";
 import avata_img_3 from "../../public/assets/img/avata/avata-3.png";
 import avata_img_4 from "../../public/assets/img/avata/avata-4.png";
@@ -27,176 +26,211 @@ import avata_img_11 from "../../public/assets/img/avata/avata-5.png";
 import avata_img_12 from "../../public/assets/img/avata/avata-6.png";
 
 const portfolio_blog = [
-    {
-        id: 1,
-        thumb_img: thumb_img_1,
-        avata_img: avata_img_1,
-        name: "Hilary Ouse",
-        job_title: "Founder & CEO Dulalix",
-        title: <>2023 Professional Year In <br /> Review</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Business",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 2,
-        thumb_img: thumb_img_2,
-        avata_img: avata_img_2,
-        name: "Rudra Ghosh",
-        job_title: "Founder & CEO Dulalix",
-        title: <>Leveraging Feedback on <br /> Insights best.</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Business",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 3,
-        thumb_img: thumb_img_3,
-        avata_img: avata_img_3,
-        name: "Geraldine",
-        job_title: "Founder & CEO Dulalix",
-        title: <>Typing Tutorials For Kids <br /> And Beginners</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Marketing",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 4,
-        thumb_img: thumb_img_4,
-        avata_img: avata_img_4,
-        name: "Helen M.",
-        job_title: "Founder & CEO Dulalix",
-        title: <>Project Management at <br /> Your fingertips.</>,
-        cls: "",
-        date: "April 24, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Designing",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 5,
-        thumb_img: thumb_img_5,
-        avata_img: avata_img_5,
-        name: "R. Jones",
-        job_title: "Founder & CEO Dulalix",
-        title: <>2023 Professional Year In <br /> Review</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Development",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 6,
-        thumb_img: thumb_img_6,
-        avata_img: avata_img_6,
-        name: "Frank P. Miller",
-        job_title: "Founder & CEO Dulalix",
-        title: <>How Chatbots Can Help You <br /> Drive More Sales</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Resources",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    }, 
+  {
+    id: 1,
+    thumb_img: "https://ithub-img.vhiep.com/post/docker.png",
+    avata_img: "https://ithub-img.vhiep.com/an.png",
+    name: "Nguyễn Bảo Ân",
+    job_title: "Chủ nhiệm CLB",
+    title: <>Deploy dự án NodeJS với Docker</>,
+    cls: "",
+    date: "22, Th5 2024",
+    col: "",
+    delay: ".5s",
+    category: "Docker",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 2,
+    thumb_img: "https://ithub-img.vhiep.com/post/github-action.png",
+    avata_img: "https://ithub-img.vhiep.com/kaka.png",
+    name: "Cao Ka Ka",
+    job_title: "Cố vấn CLB",
+    title: <>CI/CD - GitHub Actions và các kiến thức cơ bản</>,
+    cls: "",
+    date: "22, Th5 2024",
+    col: "",
+    delay: ".5s",
+    category: "Github",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 3,
+    thumb_img: "https://ithub-img.vhiep.com/post/figma.png",
+    avata_img: "https://ithub-img.vhiep.com/loc.png",
+    name: "Phạm Hữu Lộc",
+    job_title: "Frontend Leader",
+    title: <>Sử dụng Figma để thiết kế giao diện</>,
+    cls: "",
+    date: "21, Th5 2024",
+    col: "",
+    delay: ".5s",
+    category: "Design",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 4,
+    thumb_img: thumb_img_4,
+    avata_img: avata_img_4,
+    name: "Helen M.",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        Project Management at <br /> Your fingertips.
+      </>
+    ),
+    cls: "",
+    date: "April 24, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Designing",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 5,
+    thumb_img: thumb_img_5,
+    avata_img: avata_img_5,
+    name: "R. Jones",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        2023 Professional Year In <br /> Review
+      </>
+    ),
+    cls: "",
+    date: "April 12, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Development",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 6,
+    thumb_img: thumb_img_6,
+    avata_img: avata_img_6,
+    name: "Frank P. Miller",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        How Chatbots Can Help You <br /> Drive More Sales
+      </>
+    ),
+    cls: "",
+    date: "April 12, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Resources",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
 
-    // extra
-    {
-        id: 7,
-        thumb_img: thumb_img_7,
-        avata_img: avata_img_7,
-        name: "Hilary Ouse",
-        job_title: "Founder & CEO Dulalix",
-        title: <>2023 Professional Year In <br /> Review</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Business",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 8,
-        thumb_img: thumb_img_8,
-        avata_img: avata_img_8,
-        name: "Rudra Ghosh",
-        job_title: "Founder & CEO Dulalix",
-        title: <>Leveraging Feedback on <br /> Insights best.</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Business",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 9,
-        thumb_img: thumb_img_9,
-        avata_img: avata_img_9,
-        name: "Geraldine",
-        job_title: "Founder & CEO Dulalix",
-        title: <>Typing Tutorials For Kids <br /> And Beginners</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Marketing",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 10,
-        thumb_img: thumb_img_10,
-        avata_img: avata_img_10,
-        name: "Helen M.",
-        job_title: "Founder & CEO Dulalix",
-        title: <>Project Management at <br /> Your fingertips.</>,
-        cls: "",
-        date: "April 24, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Designing",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 11,
-        thumb_img: thumb_img_11,
-        avata_img: avata_img_11,
-        name: "R. Jones",
-        job_title: "Founder & CEO Dulalix",
-        title: <>2023 Professional Year In <br /> Review</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Development",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    },
-    {
-        id: 12,
-        thumb_img: thumb_img_12,
-        avata_img: avata_img_12,
-        name: "Frank P. Miller",
-        job_title: "Founder & CEO Dulalix",
-        title: <>How Chatbots Can Help You <br /> Drive More Sales</>,
-        cls: "",
-        date: "April 12, 2023",
-        col: "",
-        delay: ".5s",
-        category: "Resources",
-        filter_id: ["all", "business", "marketing", "designing", "development", "resources" ],
-    }, 
-
-]
-export default portfolio_blog
+  // extra
+  {
+    id: 7,
+    thumb_img: thumb_img_7,
+    avata_img: avata_img_7,
+    name: "Hilary Ouse",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        2023 Professional Year In <br /> Review
+      </>
+    ),
+    cls: "",
+    date: "April 12, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Business",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 8,
+    thumb_img: thumb_img_8,
+    avata_img: avata_img_8,
+    name: "Rudra Ghosh",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        Leveraging Feedback on <br /> Insights best.
+      </>
+    ),
+    cls: "",
+    date: "April 12, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Business",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 9,
+    thumb_img: thumb_img_9,
+    avata_img: avata_img_9,
+    name: "Geraldine",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        Typing Tutorials For Kids <br /> And Beginners
+      </>
+    ),
+    cls: "",
+    date: "April 12, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Marketing",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 10,
+    thumb_img: thumb_img_10,
+    avata_img: avata_img_10,
+    name: "Helen M.",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        Project Management at <br /> Your fingertips.
+      </>
+    ),
+    cls: "",
+    date: "April 24, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Designing",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 11,
+    thumb_img: thumb_img_11,
+    avata_img: avata_img_11,
+    name: "R. Jones",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        2023 Professional Year In <br /> Review
+      </>
+    ),
+    cls: "",
+    date: "April 12, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Development",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+  {
+    id: 12,
+    thumb_img: thumb_img_12,
+    avata_img: avata_img_12,
+    name: "Frank P. Miller",
+    job_title: "Founder & CEO Dulalix",
+    title: (
+      <>
+        How Chatbots Can Help You <br /> Drive More Sales
+      </>
+    ),
+    cls: "",
+    date: "April 12, 2023",
+    col: "",
+    delay: ".5s",
+    category: "Resources",
+    filter_id: ["all", "business", "marketing", "designing", "development", "resources"],
+  },
+];
+export default portfolio_blog;
